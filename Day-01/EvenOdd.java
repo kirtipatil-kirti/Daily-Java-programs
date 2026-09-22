63b5d7a2b41e4b6e31826cc3e9df2639ce4f79c2
Kirti Patil <kirtipatil639@gmail.com>
2026-09-22 20:04:36 +0530

    Day 01:Even or odd program

---
+++ Day-01/EvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class EvenOdd{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int number=scanner.nextInt();
+        if(number%2==0){
+            System.out.println("Number is Even");
+        }
+        else{
+            System.out.println("Number is Odd");
+        }
+
+    }
+}
