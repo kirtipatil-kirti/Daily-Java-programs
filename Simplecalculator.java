897f21744f2ebcfe05a97162c25027c924cc201c
Kirti Patil <kirtipatil639@gmail.com>
2026-09-26 08:36:44 +0530

    Simple Calculator

---
+++ Simplecalculator.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Simplecalculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        char operator=sc.next().charAt(0);
+        switch(operator){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+               System.out.println(a*b);
+                break;
+            case '/':
+                if(b!=0){
+                System.out.println(a/b);
+                }else{
+                    System.out.println("Cannot divide by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+            
+        }
+        sc.close();
+    }
+    
+}
